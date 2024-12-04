01557ba38ffeb4e023106111478b9f9ab1b7de4b
Dubem-Umeh-Raphael <raphaelumeh21@gmail.com>
2024-12-04 02:42:58 +0000

    new sever commit

---
+++ sendEmail.js
@@ -0,0 +1,127 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const authUser = process.env.BREVO_AUTH_USER;
+const authPass = process.env.BREVO_AUTH_PASS;
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false,
+  auth: { 
+    user: authUser,
+    pass: authPass,
+  },
+});
+
+/**
+ * Send an email with the given data
+ * @param {Object} data
+ */
+  export const sendEmail = async ({ email, cantMakeIt, guestNames, wellWishingMessage }) => {
+  // Checking for cantMakeIt condition
+  const isCantMakeIt = typeof cantMakeIt === 'boolean' ? cantMakeIt : cantMakeIt === 'true';
+
+  // Get the first Name of the guest
+  const firstName = guestNames[0]?.firstName || 'Guest';
+  const lastName = guestNames[0]?.lastName || '';
+
+  // For Google calender reminder
+  const eventDate = '20250104T090000Z'; // Use UTC format: YYYYMMDDTHHmmssZ
+  const eventEndDate = '20250104T150000Z'; // End time in UTC format
+  const eventTitle = 'M & C 2025 Wedding';
+  const eventLocation = 'Holy Name Parish, Umuchu';
+  const eventDescription = 'Join us for the wedding celebration of M & C!';
+
+  const googleCalendarLink = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(eventTitle)}&dates=${eventDate}/${eventEndDate}&details=${encodeURIComponent(eventDescription)}&location=${encodeURIComponent(eventLocation)}`;
+
+  // Create the email body based on CantMakeIt condition
+  const emailBody = `
+    <body>
+      <div style='font-size:18px; color:#fff;'>
+        <p>Dear ${firstName},</p>
+        ${isCantMakeIt ?
+          `
+            <p>Thank you for letting us know. We're sorry to hear that you can't make it to our wedding.</p>
+            <p>If anything changes, please feel free to update your RSVP.</p>
+            <p> <a href='http://manc2025.com/rsvp/rsvp-form'>Resend RSVP?</a> </p>
+          `
+          :
+          `
+            <p>Thank you for confirming your attendance at our wedding. We're excited to welcome you on the event day!</p>
+            <p style='margin-top=5px; margin-bottom=3px'>Here are the details:</p>
+            <ul style='color:#d3d3d3;'>
+              <li><strong>Date:</strong> Jan 4th 2025</li>
+              <li><strong>Wedding Location:</strong> <a href='https://maps.app.goo.gl/PQrqnmtmYnceX5hs7' target='_blank'>Holy Name Parish, Umuchu</a></li>
+              <li><strong>Traditional Marriage Location:</strong> <a href='https://maps.app.goo.gl/vCcLLUoLynrBaVbc7'>Umuojogwo, Umuchu</a></li>
+              <li><strong>Guests: </strong>${guestNames.map(guest => `${guest.firstName} ${guest.lastName}`).join(', ')}</li>
+              <li>Guest Count: ${guestNames.length}</li>
+            </ul>
+            <div style='margin-top:3px;'>
+              <p>You can <a href="${googleCalendarLink}" target="_blank">add this event to your Google Calendar</a> to set a reminder!</p>
+              <p>if you have any question, feel free to reach out.</p>
+            </div>
+          `
+          }
+          <p>Best regards,</p>
+          <p>The Event Team</p>
+      </div>
+    </body>
+  ` ;
+
+  // Sending the email
+  await transporter.sendMail({
+    from: '"M & C 2025" <mj.charles2025@gmail.com>',
+    to: email,
+    subject: 'Welcome to our wedding!',
+    text: `Thank you for your RSVP`,
+    html: emailBody,
+    replyTo: 'mj.charles2025@gmail.com',
+  });
+
+
+
+  // Organizer email body
+  const organizerEmailBody = ` 
+    <body>
+      <div style='font-size:18px; color:#fff;>
+        <p>Hello Event Organizer,</p>
+        <p style='margin-bottom=5px'>A new RSVP has been received:</p>
+        <ul style='margin-bottom=5px'>
+          <li><strong>Guest Name:</strong> ${firstName} ${lastName}</li>
+          <li><strong>Email:</strong> ${email}</li>
+        </ul>
+        ${cantMakeIt ?
+          `<p><strong>Status:</strong> The guest has indicated they <strong>Cannot Attend</strong>.</p>`
+          :
+          `<p><strong>Status:</strong> The guest has confirmed they <strong>will be attending</strong>.</p>
+          <ul>
+            <li><strong>Guests: </strong>${guestNames.map(guest => `${guest.firstName} ${guest.lastName}`).join(', ')}</li>
+            <li>Guest Count: ${guestNames.length}</li>
+          </ul>
+          `
+        }
+        ${wellWishingMessage ?
+          `<p><strong>Well-Wishing Message:</strong></p>
+          <p>${wellWishingMessage}</p>`
+          :
+          `<p><strong>Well-Wishing Message:</strong> No message provided.</p>`
+        }
+        <p>Best regards,</p>
+        <p>RSVP System</p>
+      </div>
+    </body>
+  `;
+
+  // Sending the email to the organizer
+  await transporter.sendMail({
+    from: '"M & C 2025" <mj.charles2025@gmail.com>',
+    to: 'mj.charles2025@gmail.com', // Organizer's email address
+    subject: 'New RSVP Response',
+    text: `A new RSVP from ${firstName} has been received`,
+    html: organizerEmailBody,
+    replyTo: 'mj.charles2025@gmail.com',
+  });
+};
---
+++ server2.js
@@ -0,0 +1,31 @@
+// backend/server2.js
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { sendEmail } from './sendEmail.js'; // Ensure this path is correct
+
+const app = express();
+const PORT = 3500;
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/send-rsvp', async (req, res) => {
+  const { email, guestNames, cantMakeIt, wellWishingMessage } = req.body;
+
+  try {
+    await sendEmail({
+      email,
+      wellWishingMessage,
+      guestNames,
+      cantMakeIt,
+    });
+    res.status(200).json({ success: true, message: "RSVP submitted and email sent successfully!" });
+  } catch (error) {
+    console.error("Error sending RSVP email:", error);
+    res.status(500).json({ success: false, message: "There was a problem submitting your RSVP. Please try again later." });
+  }
+});
+
+app.listen(3500, () => {
+  console.log(`Server running on ${PORT}`);
+});
